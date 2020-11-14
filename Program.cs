8a201a939aed51ebcaad02de16584b8014364e56
Ali GOREN <goren.ali@yandex.com>
2020-11-14 13:08:04 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Serilog;
+
+namespace serilog_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://localhost:5341")
+                .Enrich.WithProperty("AppName", "serilog_project")
+                .CreateLogger();
+            
+            string adSoyad = "Ali GOREN";
+            
+            Log.Information("{adSoyad} giriş yaptı", adSoyad);
+
+            Log.Error("{adSoyad} onceden yasaklanmis bir kullanici!", adSoyad);
+
+            Log.CloseAndFlush();
+            
+        }
+    }
+}
